308f8d74e5e14a87c6a45e8485033e427853efe7
JoileJr <joilejunior2@'=gmail.com>
2023-05-23 22:26:29 -0300

    lendo csv

--- backend/src/main/java/com/dev/backend/controller/EstadoController.java
+++ backend/src/main/java/com/dev/backend/controller/EstadoController.java
@@ -1,8 +1,10 @@
 package com.dev.backend.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +13,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.dev.backend.entity.Estado;
 import com.dev.backend.service.EstadoService;
+import com.opencsv.exceptions.CsvException;
 
 @RestController
 @RequestMapping("/api/estado")
@@ -26,6 +31,17 @@ public class EstadoController {
     @GetMapping("/")
     public List<Estado> buscarTodos(){
        return estadoService.buscarTodos();
+    }
+
+    @PostMapping("/processar")
+    public ResponseEntity<String> processarCSV(@RequestParam("file") MultipartFile file) {
+        try {
+            estadoService.lerCSV(file);
+            return ResponseEntity.ok("Arquivo CSV processado com sucesso");
+        } catch (IOException | CsvException e) {
+            System.out.println("Erro ao receber o arquivo");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao processar o arquivo CSV");
+        }
     }
 
     @PostMapping("/")
--- backend/src/main/java/com/dev/backend/service/EstadoService.java
+++ backend/src/main/java/com/dev/backend/service/EstadoService.java
@@ -1,13 +1,19 @@
 package com.dev.backend.service;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.dev.backend.entity.Estado;
 import com.dev.backend.repository.EstadoRepository;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 
 @Service
 public class EstadoService {
@@ -33,5 +39,37 @@ public class EstadoService {
     public void excluir(Long id) {
         Estado estado = estadoRepository.findById(id).get();
         estadoRepository.delete(estado);
+    }
+
+
+    public List<Estado> lerCSV(MultipartFile file) throws IOException, CsvException {
+        List<Estado> estados = new ArrayList<>();
+
+        try (CSVReader reader = new CSVReader(new InputStreamReader(file.getInputStream()))) {
+            List<String[]> linhas = reader.readAll();
+
+            String[] linhaSigla = linhas.get(0);
+            if(linhaSigla[2].equals("sigla")){
+                for (int i = 1; i < linhas.size(); i++) {
+                    String[] linha = linhas.get(i);
+                    Estado estado = new Estado();
+                    long id = Long.parseLong(linha[0]);
+                    if(estadoRepository.existsById(id)){
+                        estado = estadoRepository.findById(id).get();
+                        estado.setDataAtualizacao(new Date());
+                    } else {
+                        estado.setId(id);
+                        estado.setDataCriacao(new Date());
+                    }
+                    estado.setNome(linha[1]);
+                    estado.setSigla(linha[2]);  
+                    estadoRepository.saveAndFlush(estado);
+                    estados.add(estado);
+                }
+            } else {
+                System.out.println("Esse arquivo não tem os dados correspondente");
+            }
+        }
+        return estados;
     }
 }
